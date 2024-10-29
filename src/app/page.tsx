c16000e74bf59120c95fad76065fbbc0a9018b97
saira abid <sairaabid132@gmail.com>
2024-10-30 01:59:41 +0500

    done

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+
+
+export default function Home() {
+  return (
+    <div className="bg-primary h-[400px] bg-no-repeat w-full bg-cover">
+      {/*png jpg jpeg svg webp */}
+      <h2 className="my-[0%] text-fahad text-[60px] p-1">
+        HELLO WORLD
+      </h2>
+    </div>
+  )
+}
